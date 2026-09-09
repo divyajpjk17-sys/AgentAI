5c256aff5a01b4207c0b778d6384fdd10cab80bb
Divyadarshini J <divyajpjk17@gmail.com>
2026-09-09 09:39:32 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,21 @@
-import os , urllib.parse , urlib.request , render_template
+from flask import Flask, render_template
 from app.youtube import youtube_bp
 
-Gemini_api_key = "Gemini_API_Key";
-
-def home():
-  return render_template ("index.html")
-
 def create_app():
-  app = Flask(_name_)
-  app.register_blueprint(youtube_bp, url_prefix='/youtube")
 
-@app.route("/html")
-def html():
-  return render_template("index.html")
-  
-return app;
+    app = Flask(__name__)
+
+    app.register_blueprint(
+        youtube_bp,
+        url_prefix="/youtube"
+    )
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/html")
+    def html():
+        return render_template("index.html")
+
+    return app
