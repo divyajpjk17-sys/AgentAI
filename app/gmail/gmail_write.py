b7c46fa0b03dfb7f4e3efb4665aef0b082f226ff
Divyadarshini J <divyajpjk17@gmail.com>
2026-09-10 14:16:44 +0530

    Update gmail_write.py

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -1,1 +1,12 @@
+import os 
+import re
+import urllib.parse
 
+CLIENT_EMAIL = os.getenv("CLIENT_EMAIL", "")
+
+KEYWORDS = (
+  "gmail" , "email" , "e-mail" , "mail",
+  "write an email" , "send an mail" , "draft an mail",
+  "compose an mail" , "write mail" , "send mail" , "draft mail",
+  "compose mail"
+)
