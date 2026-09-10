01bebb49cc487ea7f4dfa755fe0f4a75ecfbd4db
Divyadarshini J <divyajpjk17@gmail.com>
2026-09-10 15:09:02 +0530

    Enhance app initialization with CORS and health check
    Added CORS support and health check endpoint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, request,jsonify,render_template
+from flask_cors import CORS
+
+from app.gmail import(
+  is_email_command,
+  extract_email,
+  create_gmail_url,
+  generate_email_with_gemini
+)
+
 from app.youtube import youtube_bp
 
 def create_app():
-
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(
         youtube_bp,
@@ -11,11 +22,12 @@ def create_app():
     )
 
     @app.route("/")
-    def home():
-        return render_template("index.html")
-
-    @app.route("/html")
     def html():
         return render_template("index.html")
 
-    return app
+    @app.route("/health")
+    def health():
+        return jsonify({
+            "status" : "ok",
+        })
+
